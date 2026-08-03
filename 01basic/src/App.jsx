17cf8b0440a0ce06fa899822963d317df1a50292
student <student@example.com>
2026-08-03 22:48:18 +0530

    new file added

--- 01basic/src/App.jsx
+++ 01basic/src/App.jsx
@@ -1,9 +1,10 @@
-
+import New from './new'
 function App() {
   
 
   return (
     <>
+    <New></New>
    <p>how are you</p>
     <h1>hello guru </h1>
     </>
---
+++ 01basic/src/new.jsx
@@ -0,0 +1,6 @@
+function New(){
+    return(
+        <h1>new component</h1>
+    )
+}
+export default New
