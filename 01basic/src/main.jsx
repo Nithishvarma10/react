0fe19cad304187ff795cb1d98479394aa0d121b5
student <student@example.com>
2026-08-04 19:31:09 +0530

    new element creation

--- 01basic/src/main.jsx
+++ 01basic/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App.jsx'
+const reacter = React.createElement(
+  'a', 
+  { href: 'https://google.com', target:'_blank' },
+  'visit google'
+)
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  reacter
 )
